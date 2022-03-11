686cae7832f4ae69c9b60999c9347cac22c063e0
carlatakagi <carlatakagi@gmail.com>
2022-03-10 21:11:19 -0400

    requisito 4 - nao esta passando no teste

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Game from './pages/Game';
 import Login from './pages/Login';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/game" component={ Game } />
+      {/* adicionei apenas para teste manual - cypress ainda nao roda
+      por causa do click do botao */}
     </Switch>
   );
 }
---
+++ src/pages/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HeaderGame from '../components/HeaderGame';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <HeaderGame />
+      </div>
+    );
+  }
+}
+
+export default Game;
